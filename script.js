e85ff44b5d26eb2f9462678eaf8b3f0fe6836ef4
Theodor K <greddy119@bk.ru>
2023-05-27 12:36:49 +0300

    am creat button in index.html, am adaugat script

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const h1 = document.querySelector('h1')
 patrat.addEventListener('click', (ev) => {
     h1.style.color = 'red'
 })
+
+const body = document.body
+const adauga = document.querySelector('.createbutton')
+const element = document.createElement('div')
+element.classList.add('patrat')
+
+adauga.addEventListener('click', (ev)=>{
+    if (ev.target) {body.parentNode.appendChild(element).after(patrat)}
+})
